01a0b7c12ef056bcc910158650f36c4339d88ceb
Evert Heylen <evertheylen@gmail.com>
2017-03-11 13:51:21 +0100

    Fixed popgen CMake

--- src/main/cpp/popgen/main.cpp
+++ src/main/cpp/popgen/main.cpp
@@ -3,8 +3,6 @@
 
 #include "PopulationGenerator.h"
 
-#include "util/AliasDistribution.cpp"
-
 int main() {
-	std::cout << "Population generator placeholder";
+	std::cout << "Population generator placeholder\n";
 }
