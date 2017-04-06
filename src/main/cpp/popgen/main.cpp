605101b8af63055624d07ca128606819e46ffaba
SamMylle <mylle.sam@gmail.com>
2017-04-06 11:27:48 +0200

    Fixed main

--- src/main/cpp/popgen/main.cpp
+++ src/main/cpp/popgen/main.cpp
@@ -9,13 +9,13 @@ using namespace stride;
 using namespace popgen;
 
 int main(int argc, char** argv) {
-	if (argc <= 1) {
+	if (argc < 5) {
 		cerr << "Please provide a file as first argument.\n";
 		return 1;
 	}
 	cerr << "Starting...\n";
 	PopulationGenerator generator {argv[1]};
 	cerr << "Generating...\n";
-	generator.generate("cities.txt", "pop.txt", "hh.txt");
+	generator.generate(argv[2], argv[3], argv[4]);
 	cerr << "Done!\n";
 }
