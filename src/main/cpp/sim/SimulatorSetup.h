50d963bcd015c239c0ba93d0b0fa5d6039da6abd
FedericoQuin <federico-quin@hotmail.com>
2017-04-17 03:53:52 +0200

    Added SimulatorSetup class to handle config tree and simulator building

---
+++ src/main/cpp/sim/SimulatorSetup.h
@@ -0,0 +1,46 @@
+
+/**
+ * @file
+ * Setup for the simulator and configuration tree.
+ */
+
+
+#pragma once
+
+#include <boost/property_tree/xml_parser.hpp>
+#include <string>
+#include <memory>
+#include "sim/Simulator.h"
+
+using namespace boost::property_tree;
+using namespace std;
+
+namespace stride {
+
+class SimulatorSetup {
+public:
+	SimulatorSetup(string simulator_mode, string conf_file, string hdf5_file, int num_threads, bool track_index_case);
+	
+	ptree getConfigTree() const { 
+		return m_pt_config; 
+	}
+	shared_ptr<Simulator> getSimulator() const;
+
+private:
+	void constructConfigTreeInitial();
+	void constructConfigTreeExtend();
+
+	/// Helper function to check if the file with filename actually exists.
+	bool fileExists(string filename) const;
+
+	string		m_simulator_mode;
+	string		m_conf_file;
+	string		m_hdf5_file;
+	int			m_num_threads;
+	bool		m_track_index_case;
+	bool		m_conf_file_exists;
+	bool 		m_hdf5_file_exists;
+	ptree 		m_pt_config;
+};
+
+}
