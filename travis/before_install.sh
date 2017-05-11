da1f1c9aabdd33cfb69eb3aa28d9d460f0d31342
Evert Heylen <evertheylen@gmail.com>
2017-05-12 00:57:14 +0200

    Travis: set permissions correctly and better naming

---
+++ travis/before_install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Expand all environment shorthands
+
+echo travis_fold:start:expand_environment
+echo Expanding Environment variables
+
+# compiler
+if [ "$compiler" = "gcc" ]; then
+	export CXX=g++-5
+	export CC=gcc-5
+elif [ "$compiler" = "clang" ]; then
+	export CXX=clang++-3.8
+	export CC=clang-3.8
+else
+	echo "unrecognized option: $compiler"
+	exit 2
+fi
+
+# unipar
+if [ "$unipar" = "TBB" ]; then
+	export STRIDE_UNIPAR=TBB
+elif [ "$unipar" = "OpenMP" ]; then
+	export STRIDE_UNIPAR=OPENMP
+	export OMP_NUM_THREADS=4
+elif [ "$unipar" = "dummy" ]; then
+	export STRIDE_UNIPAR=DUMMY
+else
+	echo "unrecognized option: $unipar"
+	exit 2
+fi
+
+# test
+if [ "$test" = "unit" ]; then
+	export GTEST_FILTER="*UnitTests__*"
+elif [ "$test" = "scenarios" ]; then
+	export GTEST_FILTER="*Scenarios__*"
+else
+	echo "Running all tests"
+	export GTEST_FILTER="*"
+fi
+
+echo travis_fold:end:expand_environment
