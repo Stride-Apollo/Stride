6601955d8065cbdbbfddb836d8dda347dd2f8be7
Evert Heylen <evertheylen@gmail.com>
2017-05-13 17:13:32 +0200

    Adjusted cutoff points for code quality [ci skip]

--- travis/code_quality.sh
+++ travis/code_quality.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+if [ "${quality_dir}" = "src/main" ]; then
+	# Be very strict on normal code
+	MINLINES=5
+	LIMIT=2
+else
+	MINLINES=15
+	LIMIT=5
+fi
+
 jscpd \
 	--path ${quality_dir:=.} \
 	--skip-comments \
 	--languages clike,javascript,python,css,htmlmixed \
 	--exclude "**/resources/**" \
-	--min-lines 15 \
+	--min-lines $MINLINES \
 	--blame \
-	--limit 5
+	--limit $LIMIT
