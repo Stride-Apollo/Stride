1182f963c8d05693d3d7830f2336b54fe51af7af
Stijn Janssens <janssens.stijn@hotmail.com>
2017-06-23 16:29:14 +0200

    Zoom optimize + configurable

--- src/main/electron/index.js
+++ src/main/electron/index.js
@@ -13,13 +13,15 @@ app.controller('Controller', ['$scope', '$timeout', '$interval', function($scope
 	$scope.currentDay = 0;
 
 	$scope.save = function() {
+		console.log("Saving");
 		var saveString = "{\"directory\": \"" + config.directory.toString() + "\",\"color_no_infected\": \""
 			+ getHexColor($scope.no_infected_color).toString() + "\",\"color_min_infected\": \""
 			+ getHexColor($scope.min_infected_color).toString() + "\",\"color_max_infected\": \""
 			+ getHexColor($scope.max_infected_color).toString() + "\",\"circle_opacity\": "
 			+ $scope.opacity + ",\"unzoomed_min_size\": " + $scope.unzoomed_min
 			+ ",\"unzoomed_max_size\":  " + $scope.unzoomed_max
-			+ ",\"animation_step\": " + $scope.animation_speed + "}";
+			+ ",\"animation_step\": " + $scope.animation_speed
+			+ ",\"boundary\": " + $scope.boundary + "}";
 		fs.writeFileSync(__dirname + "/data/config.json", saveString);
 	}
 
@@ -56,6 +58,7 @@ app.controller('Controller', ['$scope', '$timeout', '$interval', function($scope
 		$scope.unzoomed_max = config.unzoomed_max_size;
 		$scope.opacity = config.circle_opacity;
 		$scope.animation_speed = config.animation_step;
+		$scope.boundary = config.boundary;
 		//$scope.$apply();
 		addClass(document.getElementById("opacitySlider"), "is-dirty");
 		removeClass(document.getElementById("opacitySlider"), "ng-untouched");
@@ -378,7 +381,7 @@ app.controller('Controller', ['$scope', '$timeout', '$interval', function($scope
 				min_lat = coords[i][1];
 			};
 		};
-		map.fitBounds([[min_lon-2, min_lat-2], [max_lon+2, max_lat+2]]);
+		map.fitBounds([[min_lon-$scope.boundary, min_lat-$scope.boundary], [max_lon+$scope.boundary, max_lat+$scope.boundary]]);
 	}
 
 	function getSize(cluster_data) {
