e227342a00f365da259f36224945a2bf540f09a3
SamMylle <mylle.sam@gmail.com>
2017-06-24 17:28:59 +0200

    Travel scheduler tests

--- src/test/cpp/gtester/TravelSchedulerTest.cpp
+++ src/test/cpp/gtester/TravelSchedulerTest.cpp
@@ -14,25 +14,17 @@ using namespace std;
 using namespace stride;
 using namespace util;
 using namespace boost::filesystem;
+using boost::property_tree::ptree;
 
 TEST(UnitTests__TravelSchedulerTest, happy_day) {
-	const auto file_path = InstallDirs::getDataDir() /= string("traveller_schedule.json");
-
-	std::ifstream my_file;
-	my_file.open(file_path.string());
-
-	if (my_file.bad()) {
-		throw runtime_error(string(__func__)
-							+ ">Config file " + file_path.string() + " not present. Aborting.");
-	}
-	my_file.close();
+	const auto file_path = InstallDirs::getDataDir() /= string("traveller_schedule.xml");
 
 	TravellerScheduleReader reader;
 	Schedule schedule = reader.readSchedule(file_path.string());
 
 	// Loop over all days
 	for (uint i = 0; i < 7; ++i) {
-		Flight flight = Flight(i, i + 1, 10 * i + 10, 20 * i + 20, i, "Antwerp", "ANR");
+		Flight flight = Flight(to_string(i), to_string(i + 1), 10 * i + 10, 20 * i + 20, i, "Antwerp", "ANR");
 		EXPECT_EQ(flight, schedule[i].at(0));
 
 		// Check the amount of scheduled flights
@@ -45,7 +37,7 @@ TEST(UnitTests__TravelSchedulerTest, happy_day) {
 
 	// Make sure that there can be multiple flights in one day
 	ASSERT_NO_THROW(schedule[0].at(1));
-	EXPECT_EQ(Flight(0, 1, 80, 160, 0, "Antwerp", "ANR"), schedule[0].at(1));
+	EXPECT_EQ(Flight(to_string(0), to_string(1), 80, 160, 0, "Antwerp", "ANR"), schedule[0].at(1));
 }
 
 // TODO exception tests
